03d1c5ff53654d4810709afa164bb327ac8d7ce3
mbakalitahiri <motadi@hotmail.com>
2021-01-22 04:47:16 +0000

    observer pattern completed

--- observer/index.php
+++ observer/index.php
@@ -1,1 +1,72 @@
 <?php
+
+interface Observer
+{
+    public function update(Observable $observable);
+
+}
+
+abstract class Observable
+{
+    protected $observers = array();
+
+    public function addObserver(Observer $observer)
+    {
+        array_push($this->observers, $observer);
+    }
+
+    public function notifyObservers()
+    {
+        for ($i = 0; $i < count($this->observers); $i++) {
+            $this->observers[$i]->update($this);
+        }
+    }
+}
+
+abstract class AbstractWidget implements Observer
+{
+    protected $internalData = array();
+
+    public function update(Observable $observable)
+    {
+        // TODO: Implement update() method.
+        $this->internalData = $observable->getData();
+    }
+
+    abstract function render();
+}
+
+class Widget extends AbstractWidget
+{
+    public function render()
+    {
+        // TODO: Implement render() method.
+        echo "<h1>{$this->internalData['name']} {$this->internalData['surname']}</h1>";
+    }
+}
+
+class Datasource extends Observable
+{
+    private $internalData;
+
+    function __construct()
+    {
+        $this->internalData["name"] = "mohtadi";
+        $this->internalData["surname"] = "bakali tahiri";
+    }
+
+
+    public function getData()
+    {
+        return $this->internalData;
+    }
+
+}
+
+$data = new Datasource();
+$widget = new Widget();
+$data->addObserver($widget);
+
+$data->getData();
+$data->notifyObservers();
+$widget->render();
